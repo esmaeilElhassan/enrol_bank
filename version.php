97440e60f09b5aff77233a1459890f72a019dbc9
Wail Abualela <wailabualela@gmail.com>
2018-04-12 10:30:46 +0300

    Add files via upload

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$plugin->release = 'v3.4-r1';
+$plugin->version   = 2018041101;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2017110800;        // Requires this Moodle version
+$plugin->component = 'enrol_bank';      // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->cron      = 600;
